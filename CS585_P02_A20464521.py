7efc49e946d989b50f71bbee47527641a2d58d6d
unknown <kpetkov@hawk.iit.edu>
2024-03-24 19:55:42 -0500

    Finished

--- CS585_P02_A20464521.py
+++ CS585_P02_A20464521.py
@@ -13,7 +13,7 @@ else:
 
 
 
-
+# read data
 data_pre = pd.read_csv("bumble_google_play_reviews.csv")
 data = data_pre.dropna(subset=['content']).reset_index(drop=True)
 data['review_class'] = data['score'].apply(lambda x: 'positive' if x > 2 else 'negative')
@@ -25,6 +25,7 @@ train_index = int(len(data) * float(TRAIN_SIZE / 100.0))
 train_data = data.iloc[:train_index]
 test_data = data.iloc[test_index:]
 
+# global variables
 unique_vocabulary = []
 prior_probabilities = {}
 translation_table = []
@@ -35,8 +36,9 @@ pos_indices = []
 neg_indices = []
 
 
-### Naive Bayes without stop words
+### Naive Bayes without stop words, punctuation, or lowercasing
 
+# train the naive bayes classifier
 def naive_bayes_train_no_stop_words(train_data):
     global unique_vocabulary
     global prior_probabilities
@@ -47,12 +49,14 @@ def naive_bayes_train_no_stop_words(train_data):
     global pos_indices
     global neg_indices
     
+    # get indices of positive and negative reviews
     for x in range(len(train_data)):
         if train_data['review_class'][x] == 'positive':
             pos_indices.append(x)
         if train_data['review_class'][x] == 'negative':
             neg_indices.append(x)
     
+    # prior probabilities
     prior_probabilities = {
         'positive': len(train_data[train_data['review_class'] == 'positive']) / len(train_data),
         'negative': len(train_data[train_data['review_class'] == 'negative']) / len(train_data)
@@ -62,9 +66,10 @@ def naive_bayes_train_no_stop_words(train_data):
     # list of common stop words
     stop_words = ['a', 'an', 'and', 'are', 'as', 'at', 'be', 'but', 'by', 'for', 'if', 'in', 'into', 'is', 'it', 'no', 'not', 'of', 'on', 'or', 'such', 'that', 'the', 'their', 'then', 'there', 'these', 'they', 'this', 'to', 'was', 'will', 'with']
 
-
+    # translation table to remove punctuation
     translation_table = str.maketrans('', '', string.punctuation)
 
+    # list of unique words in the vocabulary
     for x in range(len(data['content'])):
         content = data['content'][x]
         lowercase_content = content.lower()
@@ -72,11 +77,12 @@ def naive_bayes_train_no_stop_words(train_data):
         vocabulary.extend(content_no_punct.split())
 
 
-
+    # remove stop words and duplicates
     for item in vocabulary:
         if item not in unique_vocabulary and item not in stop_words:
             unique_vocabulary.append(item)
-
+            
+    # binary bag of words
     binary_bag = [[0]*len(unique_vocabulary) for _ in range(len(train_data))]
 
     for x in range(len(train_data)):
@@ -86,8 +92,7 @@ def naive_bayes_train_no_stop_words(train_data):
                 index_of_word = unique_vocabulary.index(words_in_one_sentence)
                 binary_bag[x][index_of_word] = 1
 
-    # sigma(z) = 1 / (1 + e^(-z))
-
+    # number of documents including a word
     num_docs_including_word = [0]*len(unique_vocabulary)
     num_pos_docs_including_word = [0]*len(unique_vocabulary)
     num_neg_docs_including_word = [0]*len(unique_vocabulary)
@@ -112,9 +117,10 @@ def naive_bayes_train_no_stop_words(train_data):
         prob_word_given_pos_class.append(float(num_pos_docs_including_word[x] + 1) / float(count_pos_class + len(unique_vocabulary)))
         prob_word_given_neg_class.append(float(num_neg_docs_including_word[x] + 1) / float(count_neg_class + len(unique_vocabulary)))        
     
-    
+# test data reset index to avoid errors in the classifier
 test_data = test_data.reset_index(drop=True)
 
+# naive bayes classifier without stop words
 def naive_bayes_classifier_no_stop_words(test_data):
     predictions = []
     # number of unique words in the vocabulary
@@ -126,17 +132,18 @@ def naive_bayes_classifier_no_stop_words(test_data):
         pos_prob = math.log(prior_probabilities['positive'])
         neg_prob = math.log(prior_probabilities['negative'])
         document_words = test_data['content'][x].split()
-
+        
+        # calculate probabilities for each word in the review
         for word in document_words:
             word = word.lower().translate(translation_table)
             if word != '' and word not in stop_words:
                 if word in unique_vocabulary:
                     index_of_word = unique_vocabulary.index(word)
-                    # Calculate word probability with smoothing
+                    # calculate word probability with smoothing
                     word_pos_prob = (num_pos_docs_including_word[index_of_word] + alpha) / (len(pos_indices) + alpha * V)
                     word_neg_prob = (num_neg_docs_including_word[index_of_word] + alpha) / (len(neg_indices) + alpha * V)
                 else:
-                    # Apply smoothing for words not in the vocabulary
+                    # apply smoothing for words not in the vocabulary
                     word_pos_prob = alpha / (len(pos_indices) + alpha * V)
                     word_neg_prob = alpha / (len(neg_indices) + alpha * V)
                 
@@ -147,6 +154,7 @@ def naive_bayes_classifier_no_stop_words(test_data):
                 pos_prob = 10**pos_prob
                 neg_prob = 10**neg_prob
                 
+        # classify the review as positive or negative
         if pos_prob >= neg_prob:
             predictions.append('positive')
         else:
@@ -164,6 +172,7 @@ def naive_bayes_individual_no_stop_words(review):
     neg_prob = math.log(prior_probabilities['negative'])
     document_words = review.split()
 
+    # calculate probabilities for each word in the review
     for word in document_words:
         word = word.lower().translate(translation_table)
         if word != '' and word not in stop_words: 
@@ -181,16 +190,12 @@ def naive_bayes_individual_no_stop_words(review):
             # convert to linear space so that we can compare the probabilities (add up to 1)
             pos_prob = 10**pos_prob
             neg_prob = 10**neg_prob
-
-    # if pos_prob >= neg_prob:
-    #     return 'positive'
-    # else:
-    #     return 'negative'
     return pos_prob, neg_prob
     
 
 ### Naive Bayes with stop words
 
+# train the naive bayes classifier
 def naive_bayes_train(train_data):
     global unique_vocabulary
     global prior_probabilities
@@ -199,6 +204,7 @@ def naive_bayes_train(train_data):
     global pos_indices
     global neg_indices
     
+    # get indices of positive and negative reviews
     for x in range(len(train_data)):
         if train_data['review_class'][x] == 'positive':
             pos_indices.append(x)
@@ -217,11 +223,12 @@ def naive_bayes_train(train_data):
         vocabulary.extend(content.split())
 
 
-
+    # remove duplicates
     for item in vocabulary:
         if item not in unique_vocabulary:
             unique_vocabulary.append(item)
 
+    # binary bag of words
     binary_bag = [[0]*len(unique_vocabulary) for _ in range(len(train_data))]
 
     for x in range(len(train_data)):
@@ -231,6 +238,7 @@ def naive_bayes_train(train_data):
                 index_of_word = unique_vocabulary.index(words_in_one_sentence)
                 binary_bag[x][index_of_word] = 1
 
+    # number of documents including a word
     num_docs_including_word = [0]*len(unique_vocabulary)
     num_pos_docs_including_word = [0]*len(unique_vocabulary)
     num_neg_docs_including_word = [0]*len(unique_vocabulary)
@@ -250,14 +258,15 @@ def naive_bayes_train(train_data):
     count_pos_class = math.fsum(num_pos_docs_including_word)
     count_neg_class = math.fsum(num_neg_docs_including_word)
 
-
+    # calculate probabilities for each word in the vocabulary
     for x in range(len(unique_vocabulary)):
         prob_word_given_pos_class.append(float(num_pos_docs_including_word[x] + 1) / float(count_pos_class + len(unique_vocabulary)))
         prob_word_given_neg_class.append(float(num_neg_docs_including_word[x] + 1) / float(count_neg_class + len(unique_vocabulary)))        
     
-    
+# test data reset index to avoid errors in the classifier
 test_data = test_data.reset_index(drop=True)
 
+# naive bayes classifier
 def naive_bayes_classifier(test_data):
     predictions = []
     # number of unique words in the vocabulary
@@ -265,11 +274,12 @@ def naive_bayes_classifier(test_data):
     # smoothing factor
     alpha = 1
     
+    # calculate probabilities for each word in the review
     for x in range(len(test_data)):
         pos_prob = math.log(prior_probabilities['positive'])
         neg_prob = math.log(prior_probabilities['negative'])
         document_words = test_data['content'][x].split()
-
+        # calculate probabilities for each word in the review
         for word in document_words:
             if word != '':
                 if word in unique_vocabulary:
@@ -288,7 +298,7 @@ def naive_bayes_classifier(test_data):
                 # convert to linear space so that we can compare the probabilities (add up to 1)
                 pos_prob = 10**pos_prob
                 neg_prob = 10**neg_prob
-                
+        # classify the review as positive or negative  
         if pos_prob >= neg_prob:
             predictions.append('positive')
         else:
@@ -302,10 +312,12 @@ def naive_bayes_individual(review):
     # alpha is the smoothing factor
     alpha = 1
     
+    # calculate probabilities for each word in the review
     pos_prob = math.log(prior_probabilities['positive'])
     neg_prob = math.log(prior_probabilities['negative'])
     document_words = review.split()
 
+    # calculate probabilities for each word in the review
     for word in document_words:
         if word != '': 
             if word in unique_vocabulary:
@@ -323,14 +335,9 @@ def naive_bayes_individual(review):
             pos_prob = 10**pos_prob
             neg_prob = 10**neg_prob
 
-    # if pos_prob >= neg_prob:
-    #     return 'positive'
-    # else:
-    #     return 'negative'
     return pos_prob, neg_prob
 
 ### METRICS
-
 def calculate_metrics(predictions, actual_labels):
     # Initialize counters
     TP = FP = TN = FN = 0
@@ -370,49 +377,9 @@ def calculate_metrics(predictions, actual_labels):
     
 print("Petkov, Kamen, A20464521 solution:")
 print(f"Training set size: {TRAIN_SIZE}%\n")
-
-# print("Training classifier...")
-# ### Predictions no stop words
-# # train
-# naive_bayes_train_no_stop_words(train_data)
-
-# print("Testing classifier...")
-# print("Test results / metrics:")
-# # extract actual labels
-# actual_labels = test_data['review_class'].tolist()
-
-
-# # get predictions
-# predictions_no_stop_words = naive_bayes_classifier_no_stop_words(test_data)
-
-# metrics_no_stop_words = calculate_metrics(predictions_no_stop_words, actual_labels)
-# for metric, value in metrics_no_stop_words.items():
-#     print(f"{metric}: {value}")
-
-
-
-    
-# # ask user for input of a sentence
-# sentence = input("Enter your sentence: ")
-# classified_as = ""
-# # display classifier decision along with P(CLASS_A |S) and P(CLASS_B | S) values on screen
-# pos_prob_no_stop_words, neg_prob_no_stop_words = naive_bayes_individual_no_stop_words(sentence)
-# if pos_prob_no_stop_words >= neg_prob_no_stop_words:
-#     classified_as = "positive"
-# else:
-#     classified_as = "negative"
-# print("Sentence S: \n", sentence, "\nwas classified as: ", classified_as)
-
-
-# print(f"P(Positive | S): {pos_prob_no_stop_words}")   
-# print(f"P(Negative | S): {neg_prob_no_stop_words}")
     
     
-    
-    
-    
-    
-### Predictions with no improved naive bayes
+### Predictions without removing stop-words, lowercasing, or removing punctuation - naive bayes classifier
 # train
 print("Training classifier...")
 naive_bayes_train(train_data)
@@ -450,11 +417,6 @@ print(f"P(Negative | S): {neg_prob}")
 
 
 
-
-
-
-
-
 new_sentence_bool = input("Do you want to enter another sentece [Y/N]?")
 while new_sentence_bool.lower() == 'y':
     sentence = input("Enter your sentence: ")
@@ -469,45 +431,3 @@ while new_sentence_bool.lower() == 'y':
     print(f"P(Positive | S): {pos_prob}")
     print(f"P(Negative | S): {neg_prob}")
     new_sentence_bool = input("Do you want to enter another sentece [Y/N]?")
-    
-
-
-
-
-
-
-# import matplotlib.pyplot as plt
-# from sklearn.metrics import roc_curve, auc
-
-# actual_labels = [1 if label == 'positive' else 0 for label in test_data['review_class']]
-
-# # Calculate probabilities for each model
-# probabilities_no_stop_words = [naive_bayes_individual_no_stop_words(text)[0] for text in test_data['content']]
-# probabilities = [naive_bayes_individual(text)[0] for text in test_data['content']]
-
-# # Compute ROC curve and ROC area for each model
-# fpr_no_stop_words, tpr_no_stop_words, _ = roc_curve(actual_labels, probabilities_no_stop_words)
-# roc_auc_no_stop_words = auc(fpr_no_stop_words, tpr_no_stop_words)
-
-# fpr, tpr, _ = roc_curve(actual_labels, probabilities)
-# roc_auc = auc(fpr, tpr)
-
-# # Plotting
-# plt.figure()
-# lw = 2
-# plt.plot(fpr_no_stop_words, tpr_no_stop_words, color='darkorange', lw=lw, label='Strict Model (area = %0.2f)' % roc_auc_no_stop_words)
-# plt.plot(fpr, tpr, color='darkblue', lw=lw, label='Normal (Untouched) Model (area = %0.2f)' % roc_auc)
-
-# plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-# plt.xlim([0.0, 1.0])
-# plt.ylim([0.0, 1.05])
-# plt.xlabel('False Positive Rate')
-# plt.ylabel('True Positive Rate')
-# plt.title('Receiver Operating Characteristic Comparison')
-# plt.legend(loc="lower right")
-# plt.show()
-
-# print("ROC curve for the model without stop words:")
-# print(f"AUC: {roc_auc_no_stop_words:.2f}")
-# print("ROC curve for the model with stop words:")
-# print(f"AUC: {roc_auc:.2f}")
